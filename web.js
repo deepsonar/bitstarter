5fd12e07c1331e9600924da340af38f1ad2baa55
Shashi Sastry <deepsonar@yahoo.com>
2013-07-13 19:51:42 +0000

    Updated web.js2

--- web.js
+++ web.js
@@ -5,15 +5,13 @@ var data = new Buffer(256);
 
 app.get('/', function(request, response) {
   fs = require('fs');
-  fs.readFileSync('index.html', 'utf-8', function (err, data) {
-    if (err) {
-       return console.log(err);
-    }
-    response.send('data');
-  });
+  data = fs.readFileSync("/home/ubuntu/bitstarter/index.html", "utf-8"); 
+  console.log(data);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
